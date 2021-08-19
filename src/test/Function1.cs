159c8aa0434ba9515ad1125bcca7cfe0307599cc
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-08-19 18:21:09 +0200

    test project config

--- src/test/Function1.cs
+++ src/test/Function1.cs
@@ -1,31 +1,28 @@
-namespace test
-{
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Microsoft.Azure.Functions.Worker;
-    using NServiceBus;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Microsoft.Azure.Functions.Worker;
+//using NServiceBus;
 
-    public class Function1
-    {
-        IFunctionEndpoint endpoint;
+//public class Function1
+//{
+//    IFunctionEndpoint endpoint;
 
-        public Function1(IFunctionEndpoint endpoint)
-        {
-            this.endpoint = endpoint;
-        }
+//    public Function1(IFunctionEndpoint endpoint)
+//    {
+//        this.endpoint = endpoint;
+//    }
 
 
-        [Function("Function1")]
-        public async Task Run(
-            [ServiceBusTrigger("ASBTriggerQueue")] byte[] messageBody,
-            IDictionary<string, string> userProperties,
-            string messageId,
-            int deliveryCount,
-            string replyTo,
-            string correlationId,
-            FunctionContext context)
-        {
-            await endpoint.Process(messageBody, userProperties, messageId, deliveryCount, replyTo, correlationId, context);
-        }
-    }
-}
+//    [Function("Function1")]
+//    public async Task Run(
+//        [ServiceBusTrigger("ASBTriggerQueue")] byte[] messageBody,
+//        IDictionary<string, string> userProperties,
+//        string messageId,
+//        int deliveryCount,
+//        string replyTo,
+//        string correlationId,
+//        FunctionContext context)
+//    {
+//        await endpoint.Process(messageBody, userProperties, messageId, deliveryCount, replyTo, correlationId, context);
+//    }
+//}
--- src/test/Program.cs
+++ src/test/Program.cs
@@ -1,3 +1,7 @@
+using NServiceBus;
+
+[assembly: NServiceBusTriggerFunction("ASBTriggerQueue")]
+
 namespace test
 {
     using Microsoft.Extensions.Hosting;
