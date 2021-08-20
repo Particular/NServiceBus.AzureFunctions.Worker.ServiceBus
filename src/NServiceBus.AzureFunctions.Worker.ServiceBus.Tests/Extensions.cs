0dac10e245d9b5b9406b0710efb2fa447f012e2c
Tim Bussmann <timbussmann@users.noreply.github.com>
2021-08-20 15:18:24 +0200

    adding tests project

---
+++ src/NServiceBus.AzureFunctions.Worker.ServiceBus.Tests/Extensions.cs
@@ -0,0 +1,56 @@
+﻿namespace ServiceBus.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using NServiceBus.AcceptanceTesting.Support;
+    using NServiceBus.Hosting.Helpers;
+
+    public static class Extensions
+    {
+        public static IEnumerable<Type> GetTypesScopedByTestClass(this EndpointCustomizationConfiguration endpointConfiguration)
+        {
+            var types = endpointConfiguration.BuilderType.GetTypesScopedByTestClass();
+            types = types.Union(endpointConfiguration.TypesToInclude);
+            return types.Where(t => !endpointConfiguration.TypesToExclude.Contains(t)).ToList();
+        }
+
+        public static IEnumerable<Type> GetTypesScopedByTestClass(this Type componentType)
+        {
+            var scanner = new AssemblyScanner { ThrowExceptions = false };
+            var assemblies = scanner.GetScannableAssemblies();
+
+            var assembliesToScan = assemblies.Assemblies
+                //exclude acceptance tests by default
+                .Where(a => a != Assembly.GetExecutingAssembly()).ToList();
+            var types = assembliesToScan
+                .SelectMany(a => a.GetTypes());
+
+            types = types.Union(GetNestedTypeRecursive(componentType.DeclaringType, componentType));
+
+
+            return types.ToList();
+        }
+
+        static IEnumerable<Type> GetNestedTypeRecursive(Type rootType, Type builderType)
+        {
+            if (rootType == null)
+            {
+                throw new InvalidOperationException("Make sure you nest the endpoint infrastructure inside the TestFixture as nested classes");
+            }
+
+            yield return rootType;
+
+            if (typeof(IEndpointConfigurationFactory).IsAssignableFrom(rootType) && rootType != builderType)
+            {
+                yield break;
+            }
+
+            foreach (var nestedType in rootType.GetNestedTypes(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).SelectMany(t => GetNestedTypeRecursive(t, builderType)))
+            {
+                yield return nestedType;
+            }
+        }
+    }
+}
---
+++ src/NServiceBus.AzureFunctions.Worker.ServiceBus.Tests/When_shipping_handlers_in_dedicated_assembly.cs
@@ -0,0 +1,68 @@
+﻿namespace ServiceBus.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.Loader;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using NServiceBus;
+    using NServiceBus.Configuration.AdvancedExtensibility;
+    using NServiceBus.Unicast;
+    using NUnit.Framework;
+
+    public class When_shipping_handlers_in_dedicated_assembly
+    {
+        [Test]
+        public async Task Should_load_handlers_from_assembly_when_using_FunctionsHostBuilder()
+        {
+            // The message handler assembly shouldn't be loaded at this point because there is no reference in the code to it.
+            Assert.False(AppDomain.CurrentDomain.GetAssemblies().Any(a => a.FullName == "Testing.Handlers, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+
+            var serviceCollection = new ServiceCollection();
+
+            var configuration = new ServiceBusTriggeredEndpointConfiguration("assemblyTest");
+            configuration.UseSerialization<XmlSerializer>();
+            configuration.EndpointConfiguration.UsePersistence<LearningPersistence>();
+
+            var scanner = configuration.EndpointConfiguration.AssemblyScanner();
+            scanner.ThrowExceptions = false;
+
+            var settings = configuration.AdvancedConfiguration.GetSettings();
+
+            var endpointFactory = FunctionsHostBuilderExtensions.Configure(configuration, serviceCollection);
+
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var endpoint = endpointFactory(serviceProvider);
+
+
+            // we need to process an actual message to have the endpoint being created
+            await endpoint.Process(
+                Encoding.UTF8.GetBytes("<DummyMessage/>"),
+                new Dictionary<string, string>()
+                {
+                    {Headers.EnclosedMessageTypes, "Testing.Handlers.DummyMessage" }
+                },
+                Guid.NewGuid().ToString("N"),
+                1,
+                null,
+                null,
+                new FakeFunctionContext());
+
+            // The message handler assembly should be loaded now because scanning should find and load the handler assembly
+            Assert.True(AppDomain.CurrentDomain.GetAssemblies().Any(a => a.FullName == "Testing.Handlers, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+
+            // Verify the handler and message type have been identified and loaded:
+            var registry = settings.Get<MessageHandlerRegistry>();
+            var dummyMessageType = registry.GetMessageTypes().FirstOrDefault(t => t.FullName == "Testing.Handlers.DummyMessage");
+            Assert.NotNull(dummyMessageType);
+            var dummyMessageHandler = registry.GetHandlersFor(dummyMessageType).SingleOrDefault();
+            Assert.AreEqual("Testing.Handlers.DummyMessageHandler", dummyMessageHandler.HandlerType.FullName);
+
+            // ensure the assembly is loaded into the right context
+            Assert.AreEqual(AssemblyLoadContext.GetLoadContext(Assembly.GetExecutingAssembly()), AssemblyLoadContext.GetLoadContext(dummyMessageType.Assembly));
+        }
+    }
+}
