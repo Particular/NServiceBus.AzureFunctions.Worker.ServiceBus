bbf77493a588381bef8158b0516b52edd10d3633
aleksandr-samila <aleks.samila@particular.net>
2022-01-19 14:52:13 +0200

    Restored obsolete APIs `ServiceBusTriggeredEndpointConfiguration` cunstructors  which are being removed

--- src/NServiceBus.AzureFunctions.Worker.ServiceBus/obsoletes-v4.cs
+++ src/NServiceBus.AzureFunctions.Worker.ServiceBus/obsoletes-v4.cs
@@ -2,11 +2,55 @@
 
 namespace NServiceBus
 {
+    using Microsoft.Extensions.Configuration;
+
+    //using Microsoft.Extensions.Configuration;
+
     public partial class ServiceBusTriggeredEndpointConfiguration
     {
         [ObsoleteEx(ReplacementTypeOrMember = "UseNServiceBus(ENDPOINTNAME, CONNECTIONSTRING)",
-                    TreatAsErrorFromVersion = "4",
-                    RemoveInVersion = "5")]
+            TreatAsErrorFromVersion = "4",
+            RemoveInVersion = "5")]
         public string ServiceBusConnectionString { get; set; }
+
+        /// <summary>
+        /// Creates a serverless NServiceBus endpoint.
+        /// </summary>
+        [ObsoleteEx(
+            TreatAsErrorFromVersion = "4",
+            RemoveInVersion = "5")]
+        public ServiceBusTriggeredEndpointConfiguration(IConfiguration configuration)
+        {
+        }
+
+        /// <summary>
+        /// Creates a serverless NServiceBus endpoint.
+        /// </summary>
+        [ObsoleteEx(
+            TreatAsErrorFromVersion = "4",
+            RemoveInVersion = "5")]
+        public ServiceBusTriggeredEndpointConfiguration(string endpointName, IConfiguration configuration = null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a serverless NServiceBus endpoint.
+        /// </summary>
+        [ObsoleteEx(
+            TreatAsErrorFromVersion = "4",
+            RemoveInVersion = "5")]
+        public ServiceBusTriggeredEndpointConfiguration(string endpointName, string connectionStringName = null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a serverless NServiceBus endpoint.
+        /// </summary>
+        [ObsoleteEx(
+            TreatAsErrorFromVersion = "4",
+            RemoveInVersion = "5")]
+        public ServiceBusTriggeredEndpointConfiguration(string endpointName)
+        {
+        }
     }
 }
