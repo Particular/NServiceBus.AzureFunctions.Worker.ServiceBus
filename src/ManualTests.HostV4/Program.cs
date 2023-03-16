22cb1e3468689f9ab1027bc48f851ee5ae46f8cb
Tim Bussmann <timbussmann@users.noreply.github.com>
2023-03-16 18:22:39 +0100

    call installer in UseNServiceBus

--- src/ManualTests.HostV4/Program.cs
+++ src/ManualTests.HostV4/Program.cs
@@ -1,3 +1,6 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
 
@@ -8,15 +11,36 @@ public class Program
     public static void Main()
     {
         var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureFunctionsWorkerDefaults()            
             .UseNServiceBus(c =>
             {
                 c.AdvancedConfiguration.SendOnly();
 
                 c.Routing.RouteToEndpoint(typeof(TriggerMessage), "some-queue");
             })
+            .ConfigureServices(s => s.AddHostedService<MyHostedService>())
             .Build();
 
         host.Run();
+    }
+
+    class MyHostedService : IHostedService
+    {
+        IFunctionEndpoint functionEndpoint;
+
+        public MyHostedService(IFunctionEndpoint functionEndpoint)
+        {
+            this.functionEndpoint = functionEndpoint;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
     }
 }
