8fb446a7ede991bdfb88a51747b7cd22688c8cda
danielmarbach <daniel.marbach@openplace.net>
2024-05-28 10:18:26 +0200

    Simple main

--- src/ManualTests.HostV4/Program.cs
+++ src/ManualTests.HostV4/Program.cs
@@ -3,20 +3,13 @@ using NServiceBus;
 
 [assembly: NServiceBusTriggerFunction("FunctionsTestEndpoint2", TriggerFunctionName = "MyFunctionName")]
 
-public class Program
-{
-    public static void Main()
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .UseNServiceBus(c =>
     {
-        var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
-            .UseNServiceBus(c =>
-            {
-                c.Routing.RouteToEndpoint(typeof(TriggerMessage), "FunctionsTestEndpoint2");
-                c.AdvancedConfiguration.EnableInstallers();
-            })
-            .Build();
+        c.Routing.RouteToEndpoint(typeof(TriggerMessage), "FunctionsTestEndpoint2");
+        c.AdvancedConfiguration.EnableInstallers();
+    })
+    .Build();
 
-        host.Run();
-    }
-
-}
+host.Run();
