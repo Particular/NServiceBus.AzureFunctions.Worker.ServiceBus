5012af06d25e42752ba7015d514ee885a6483ffc
Andreas Öhlund <andreas.ohlund@particular.net>
2024-06-19 07:23:56 +0200

    Obsolete FunctionEndpoint in favour of IFunctionEndpoint (#526)

--- src/NServiceBus.AzureFunctions.Worker.ServiceBus/FunctionEndpoint.cs
+++ src/NServiceBus.AzureFunctions.Worker.ServiceBus/FunctionEndpoint.cs
@@ -12,6 +12,7 @@
     /// An NServiceBus endpoint hosted in Azure Function which does not receive messages automatically but only handles
     /// messages explicitly passed to it by the caller.
     /// </summary>
+    [ObsoleteEx(TreatAsErrorFromVersion = "6", RemoveInVersion = "7", ReplacementTypeOrMember = nameof(IFunctionEndpoint))]
     public class FunctionEndpoint : IFunctionEndpoint
     {
         internal FunctionEndpoint(IStartableEndpointWithExternallyManagedContainer externallyManagedContainerEndpoint, ServerlessTransport serverlessTransport, IServiceProvider serviceProvider)
