7d25d6f4d2eed35f535868332da782b1f0f6c842
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-18 19:18:10 +0200

    More spiking

--- src/MultiHost/HostBuilderExtension.cs
+++ src/MultiHost/HostBuilderExtension.cs
@@ -4,26 +4,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    // It might even be possible to source generate this entire method
+    // It might even be possible to source generate this entire method or we seperate it into a generated method and a core method
     public static void AddNServiceBus(this IServiceCollection services, Action<EndpointConfiguration>? customize = null)
     {
-        var endpointConfiguration = new EndpointConfiguration("NotEvenUsed");
-        // we still have the same problem with the transport seam not being DI aware and have to make similar workarounds
-        // unless we create an options type that has to be kept in sync with the transport configuration API
-        var transport = new AzureServiceBusTransport("TransportWillBeInitializedCorrectlyLater", TopicTopology.Default);
-        endpointConfiguration.UseTransport(transport);
-
-        var recoverability = endpointConfiguration.Recoverability();
-        recoverability.Immediate(settings => settings.NumberOfRetries(5));
-        recoverability.Delayed(settings => settings.NumberOfRetries(3));
-
-        customize?.Invoke(endpointConfiguration);
-
-        services.AddSingleton(new CommonEndpointConfigurationProvider(endpointConfiguration));
+        // Generated part?
+        services.AddKeyedSingleton<FunctionEndpoint>(new FunctionEndpoint(nameof(SalesFunction), customize ?? (_ => { })));
     }
-}
-
-public class CommonEndpointConfigurationProvider(EndpointConfiguration commonEndpointConfiguration)
-{
-    public EndpointConfiguration CommonEndpointConfiguration { get; init; } = commonEndpointConfiguration;
 }
