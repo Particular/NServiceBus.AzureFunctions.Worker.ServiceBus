9bf75fbc52b0646d046d942340882522a1c32d76
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-19 17:14:24 +0200

    Move out

--- src/MultiHost/Program.cs
+++ src/MultiHost/Program.cs
@@ -3,7 +3,6 @@ using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MultiHost;
-using NServiceBus.Configuration.AdvancedExtensibility;
 
 FunctionsApplicationBuilder builder = FunctionsApplication.CreateBuilder(args);
 builder.ConfigureFunctionsWebApplication();
@@ -20,28 +19,3 @@ builder.Services.AddNServiceBus(configuration =>
 IHost host = builder.Build();
 
 await host.RunAsync().ConfigureAwait(false);
-
-public static class EndpointConfigurationExtensions
-{
-    public static void AddHandler<THandler>(this EndpointConfiguration endpointConfiguration) => endpointConfiguration
-        .GetSettings().GetOrCreate<RegisteredHandlers>().Add(typeof(THandler));
-
-    public static void AddSaga<TSaga>(this EndpointConfiguration endpointConfiguration)
-        where TSaga : Saga
-        => endpointConfiguration
-        .GetSettings().GetOrCreate<RegisteredSagas>().Add(typeof(TSaga));
-
-    internal class RegisteredHandlers
-    {
-        public HashSet<Type> HandlerTypes { get; } = [];
-
-        public void Add(Type handlerType) => HandlerTypes.Add(handlerType);
-    }
-
-    internal class RegisteredSagas
-    {
-        public HashSet<Type> SagaTypes { get; } = [];
-
-        public void Add(Type handlerType) => SagaTypes.Add(handlerType);
-    }
-}
