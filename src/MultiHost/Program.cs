cca1655066484f705e999da35682316368817aa1
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-19 09:55:19 +0200

    Some polishing

--- src/MultiHost/Program.cs
+++ src/MultiHost/Program.cs
@@ -24,10 +24,22 @@ public static class EndpointConfigurationExtensions
     public static void AddHandler<THandler>(this EndpointConfiguration endpointConfiguration) => endpointConfiguration
         .GetSettings().GetOrCreate<RegisteredHandlers>().Add(typeof(THandler));
 
+    public static void AddSaga<TSaga>(this EndpointConfiguration endpointConfiguration)
+        where TSaga : Saga
+        => endpointConfiguration
+        .GetSettings().GetOrCreate<RegisteredSagas>().Add(typeof(TSaga));
+
     internal class RegisteredHandlers
     {
         public HashSet<Type> HandlerTypes { get; } = [];
 
         public void Add(Type handlerType) => HandlerTypes.Add(handlerType);
+    }
+
+    internal class RegisteredSagas
+    {
+        public HashSet<Type> SagaTypes { get; } = [];
+
+        public void Add(Type handlerType) => SagaTypes.Add(handlerType);
     }
 }
