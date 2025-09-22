767275fd4ddf8a34d4a8cb298591e576a5400320
David Boike <david.boike@gmail.com>
2025-09-22 15:07:49 -0500

    Example showing dependent constructors for using another service in an HTTP function

---
+++ src/MultiHost/FunctionUsingHttpFactory.cs
@@ -0,0 +1,47 @@
+namespace MultiHost;
+
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+public partial class FunctionUsingHttpFactory
+{
+    readonly IHttpClientFactory httpClientFactory;
+
+    public FunctionUsingHttpFactory(IHttpClientFactory httpClientFactory)
+    {
+        this.httpClientFactory = httpClientFactory;
+    }
+
+    [Function(nameof(FunctionUsingHttpFactory))]
+    // Currently we are bluntly keeing against the nameof(Method) but maybe we should key against the function name instead?
+    // Also for the endpoint name should we treat the function name as the endpoint name and override the queue name to be the one in the trigger
+    // for better clarity?
+    public partial Task Sales(
+        [ServiceBusTrigger("blah", Connection = "ServiceBusConnection", AutoCompleteMessages = false)]
+        Azure.Messaging.ServiceBus.ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions, CancellationToken cancellationToken = default);
+
+    partial void Configure(SalesInitializationContext context)
+    {
+        // Not important to show constructor capability
+    }
+
+    // The drawback of having this thing here is that we need to share CTOR args with the user and that might lead to troubles when they want to inject their own stuff
+    [Function("SomeApi")]
+    public async Task<HttpResponseData> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequestData request,
+        FunctionContext executionContext)
+    {
+        // with TX session enabled, these 3 operations would be "atomic"
+        await session.Send(new PlaceOrder()).ConfigureAwait(false);
+        //await session.Send(new SomeOtherMessage()).ConfigureAwait(false);
+        //await cosmosDB.SaveStuff(new Order());
+
+        // Can use HTTP factory from the constructor
+        _ = httpClientFactory.CreateClient();
+
+        return request.CreateResponse(HttpStatusCode.OK);
+    }
+}
---
+++ src/MultiHost/Gen/FunctionUsingHttpFactory.g.cs
@@ -0,0 +1,43 @@
+namespace MultiHost;
+
+using Azure.Messaging.ServiceBus;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+
+public partial class FunctionUsingHttpFactory : IConfigureEndpoint
+{
+    readonly FunctionEndpoint endpoint;
+    readonly IMessageSession session;
+
+    public FunctionUsingHttpFactory(IHttpClientFactory httpClientFactory,
+        [FromKeyedServices(nameof(Sales))] FunctionEndpoint endpoint,
+        [FromKeyedServices(nameof(Sales))] IMessageSession session)
+        : this(httpClientFactory)
+    {
+        this.endpoint = endpoint;
+        this.session = session;
+    }
+
+    public FunctionUsingHttpFactory() { }
+
+    // We could make these partial and register on the root DI. Then you can inject stuff into Configure
+    public class SalesInitializationContext : InitializationContext;
+
+    public async partial Task Sales(ServiceBusReceivedMessage message,
+        ServiceBusMessageActions messageActions, CancellationToken cancellationToken = default) =>
+        await endpoint.Process(message, messageActions, cancellationToken).ConfigureAwait(false);
+
+    partial void Configure(SalesInitializationContext context);
+
+    public void Configure(InitializationContext context)
+    {
+        switch (context)
+        {
+            case SalesInitializationContext salesContext:
+                Configure(salesContext);
+                break;
+            default:
+                break;
+        }
+    }
+}
