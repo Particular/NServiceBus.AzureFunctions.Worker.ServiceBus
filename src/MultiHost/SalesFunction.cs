c36e47e6560385866aea674678981e1d89c55731
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-22 14:43:56 +0200

    More thoughts

--- src/MultiHost/SalesFunction.cs
+++ src/MultiHost/SalesFunction.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.Functions.Worker.Http;
 public partial class SalesFunction
 {
     [Function(nameof(SalesFunction))]
+    // Currently we are bluntly keeing against the nameof(Method) but maybe we should key against the function name instead?
+    // Also for the endpoint name should we treat the function name as the endpoint name and override the queue name to be the one in the trigger
+    // for better clarity?
     public partial Task Sales(
         [ServiceBusTrigger("sales", Connection = "ServiceBusConnection", AutoCompleteMessages = false)]
         Azure.Messaging.ServiceBus.ServiceBusReceivedMessage message,
