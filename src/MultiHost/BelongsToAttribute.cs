56f8d04bcb3d3f5c33f1d4c6c89980a660921fc9
Daniel Marbach <danielmarbach@users.noreply.github.com>
2025-09-22 14:40:15 +0200

    Comment

--- src/MultiHost/BelongsToAttribute.cs
+++ src/MultiHost/BelongsToAttribute.cs
@@ -1,6 +1,6 @@
 namespace MultiHost;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)] // can something belong to multiple functions?
 public sealed class BelongsToAttribute(string functionName) : Attribute
 {
     public string FunctionName { get; } = functionName;
