f563d689e1e08c85b31e58258671e34431157b3e
Andreas Öhlund <andreas.ohlund@particular.net>
2025-10-03 09:42:06 +0200

    Cs proj cleanup

--- src/ManualTests.HostV4/Program.cs
+++ src/ManualTests.HostV4/Program.cs
@@ -1,16 +1,13 @@
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.Hosting;
 using NServiceBus;
-
-[assembly: NServiceBusTriggerFunction("FunctionsTestEndpoint2", TriggerFunctionName = "MyFunctionName")]
-
 var builder = FunctionsApplication.CreateBuilder(args);
 
-builder.AddNServiceBus(c =>
-{
-    c.Routing.RouteToEndpoint(typeof(TriggerMessage), "FunctionsTestEndpoint2");
-    c.AdvancedConfiguration.EnableInstallers();
-});
+//builder.AddNServiceBus2(c =>
+//{
+//    c.Routing.RouteToEndpoint(typeof(TriggerMessage), "FunctionsTestEndpoint2");
+//    c.AdvancedConfiguration.EnableInstallers();
+//});
 
 var host = builder.Build();
 
