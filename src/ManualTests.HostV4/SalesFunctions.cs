d71b72f7a9c85edf752ec744771d034a2b1a3564
Andreas Öhlund <andreas.ohlund@particular.net>
2025-10-06 12:40:51 +0200

    cleanup

--- src/ManualTests.HostV4/SalesFunctions.cs
+++ src/ManualTests.HostV4/SalesFunctions.cs
@@ -4,7 +4,6 @@ public class SalesFunctions : IConfigureEndpoint
 {
     public void Configure(EndpointConfiguration endpointConfiguration)
     {
-        endpointConfiguration.UseSerialization<SystemJsonSerializer>();
         endpointConfiguration.UseTransport(new LearningTransport());
     }
 }
