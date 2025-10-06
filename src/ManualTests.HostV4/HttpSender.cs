8d7192054ec3202738bf8472764638d5ffba2d9d
Andreas Öhlund <andreas.ohlund@particular.net>
2025-10-06 10:15:57 +0200

    Http trigger

--- src/ManualTests.HostV4/HttpSender.cs
+++ src/ManualTests.HostV4/HttpSender.cs
@@ -2,21 +2,22 @@
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NServiceBus;
 
-
-class HttpSender(IFunctionEndpoint functionEndpoint)
+class HttpSender([FromKeyedServices("sales")] IMessageSession messageSession)
 {
     [Function("HttpSenderV4")]
     public async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req,
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        HttpRequestData req,
         FunctionContext executionContext)
     {
         var logger = executionContext.GetLogger<HttpSender>();
         logger.LogInformation("C# HTTP trigger function received a request.");
 
-        await functionEndpoint.Send(new TriggerMessage(), executionContext)
+        await messageSession.Send(new TriggerMessage())
             .ConfigureAwait(false);
 
         var r = req.CreateResponse(HttpStatusCode.OK);
